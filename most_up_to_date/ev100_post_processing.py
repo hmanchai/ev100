709cdbe802ae48cc5878c3d08ea834c223f1948d
rpenmatc <rpenmatc@qti.qualcomm>
2021-07-09 10:13:49 -0500

    created new summary graphs

--- most_up_to_date/ev100_post_processing.py
+++ most_up_to_date/ev100_post_processing.py
@@ -1,4 +1,5 @@
 import collections
+import csv
 import glob
 import os
 import re
@@ -6,6 +7,7 @@ import numpy as np
 import pandas as pd
 import fnmatch
 import matplotlib.pyplot as plt
+import copy
 
 
 class PostProcess():
@@ -13,7 +15,7 @@ class PostProcess():
     class to hold all post processing scripts
     generates graphs and csv files
     """
-    def dlog_csv_post_process(self, base_dir, run, output_dir):
+    def dlog_csv_post_process(self, base_dir, runs, output_dir):
         """
         process individual dlog csv's to generate a summary test log csv
         :param base_dir: str
@@ -26,58 +28,59 @@ class PostProcess():
         :return output_file: str
             returns output file of csv (file path used in graph generation)
         """
-        # csv_path = os.path.join(dlog_dir, '*.csv')
-        dlog_dir = os.path.join(base_dir, 'pattern_execution (1)', 'execution_dlog', run, 'dlog')
-        pattern_names = collections.OrderedDict()
-        pattern_index = 0
+        for run in runs:
+            # csv_path = os.path.join(dlog_dir, '*.csv')
+            dlog_dir = os.path.join(base_dir, 'pattern_execution', 'execution_dlog', run, 'dlog')
+            pattern_names = collections.OrderedDict()
+            pattern_index = 0
 
-        list_results = []
-        sn_list = []
-        sn_files = {}
-        path = glob.glob(dlog_dir + "\\*\\*\\")
-        freq_mode = re.search("(.*)(\\\)(.*)(\\\)$", path[0]).group(3)
-        output_dir = os.path.join(output_dir, freq_mode, run)
+            list_results = []
+            sn_list = []
+            sn_files = {}
+            path = glob.glob(dlog_dir + "\\*\\*\\")
+            freq_mode = re.search("(.*)(\\\)(.*)(\\\)$", path[0]).group(3)
+            output_dir = os.path.join(output_dir, freq_mode, run)
 
-        self.create_folder(output_dir)
+            self.create_folder(output_dir)
 
-        for d in os.listdir(dlog_dir):
-            sn_dir = os.path.join(dlog_dir, d)
-            csv_path = sn_dir + '\\' + '*.csv'
-            list_all_csv = glob.glob(csv_path)
-            sn_files[d] = list_all_csv
-            sn_list.append(d)
-        # csv_path = "E:\johnny\dlog_csv_debugging\int_and_saf.csv"
-        sn_df = pd.DataFrame(columns=sn_list)
-        for sn, paths in sn_files.items():
-            for path in paths:
-                dlog_output = pd.read_csv(path)
-                pattern = dlog_output.at[0, 'PatternName']
-                if pattern in pattern_names:
-                    index = pattern_names.get(pattern)
-                    if int(dlog_output['Failures']) != 0:
-                        sn_df.at[index, sn] = 'F'
-                else:
-                    pattern_names[pattern] = pattern_index
-                    if int(dlog_output.at[0, 'Failures']) != 0:
-                        sn_df.at[pattern_index, sn] = 'F'
-                    if int(dlog_output.at[0,'PatternRunTime']) > 16:
-                        sn_df.at[pattern_index, sn] = 'T'
-                    pattern_index += 1
+            for d in os.listdir(dlog_dir):
+                sn_dir = os.path.join(dlog_dir, d)
+                csv_path = sn_dir + '\\' + '*.csv'
+                list_all_csv = glob.glob(csv_path)
+                sn_files[d] = list_all_csv
+                sn_list.append(d)
+            # csv_path = "E:\johnny\dlog_csv_debugging\int_and_saf.csv"
+            sn_df = pd.DataFrame(columns=sn_list)
+            for sn, paths in sn_files.items():
+                for path in paths:
+                    dlog_output = pd.read_csv(path)
+                    pattern = dlog_output.at[0, 'PatternName']
+                    if pattern in pattern_names:
+                        index = pattern_names.get(pattern)
+                        if int(dlog_output['Failures']) != 0:
+                            sn_df.at[index, sn] = 'F'
+                    else:
+                        pattern_names[pattern] = pattern_index
+                        if int(dlog_output.at[0, 'Failures']) != 0:
+                            sn_df.at[pattern_index, sn] = 'F'
+                        if int(dlog_output.at[0,'PatternRunTime']) > 16:
+                            sn_df.at[pattern_index, sn] = 'T'
+                        pattern_index += 1
 
-        pattern_rows = list(pattern_names.items())
-        df_data = pd.DataFrame(pattern_rows, columns=["Pattern Name", "Index"])
-        regex = "(tk_atpg_)(.*)"
-        df_data["DFT Type"] = df_data.apply(lambda x: re.search(regex, x["Pattern Name"]).group(2).split("_")[0].upper(),
-                                            axis=1)
-        df_data["freq mode"] = df_data.apply(lambda x: freq_mode, axis=1)
-        print(sn_df.notnull().sum(axis=1))
-        df_data["# failing"] = sn_df.notnull().sum(axis=1)
-        df_data["# failing"].fillna("0", inplace = True)
-        df_data = pd.concat([df_data, sn_df], axis = 1)
-        df_data.pop('Index')
-        output_file = os.path.join(output_dir, freq_mode + "_postprocess_failures.csv")
-        df_data.to_csv(output_file, index=False, sep=',', header=True, mode='w')
-        return output_file
+            pattern_rows = list(pattern_names.items())
+            df_data = pd.DataFrame(pattern_rows, columns=["Pattern Name", "Index"])
+            regex = "(tk_atpg_)(.*)"
+            df_data["DFT Type"] = df_data.apply(lambda x: re.search(regex, x["Pattern Name"]).group(2).split("_")[0].upper(),
+                                                axis=1)
+            df_data["freq mode"] = df_data.apply(lambda x: freq_mode, axis=1)
+
+            df_data["# failing"] = sn_df.notnull().sum(axis=1)
+            df_data["# failing"].fillna("0", inplace = True)
+            df_data = pd.concat([df_data, sn_df], axis = 1)
+            df_data.pop('Index')
+            output_file = os.path.join(output_dir, freq_mode + "_postprocess_failures.csv")
+            df_data.to_csv(output_file, index=False, sep=',', header=True, mode='w')
+
 
     def create_folder(self, dir):
         """
@@ -158,7 +161,6 @@ class PostProcess():
             freq_mode = re.search("(.*)(\\\)(.*)(\\\)(.*)$", output_path).group(3)
             run = re.search("(.*)(\\\)(.*)$", output_path).group(3)
             for dft_type in passing_rate.keys():
-                pass_fail = {"pass": "", "fail": ""}
                 total_chips = 0
                 passing = 0
                 df_vector_type = df_data[df_data['DFT Type'].str.match(dft_type.upper())]
@@ -171,9 +173,7 @@ class PostProcess():
                     total_chips += 1
                     if df_vector_type[col].isnull().sum() == df_vector_type.shape[0]:
                         passing += 1
-                pass_fail["pass"] = passing
-                pass_fail["fail"] = total_chips - passing
-                title = freq_mode + " " + dft_type + " Pass or Fail"
+
                 # plt.title(title)
                 # plt.ylabel("# of Chips")
                 # self.add_labels(list(pass_fail.keys()), list(pass_fail.values()))
@@ -191,14 +191,35 @@ class PostProcess():
                     rate_list = passing_rate.get(dft_type)
                     rate_list.append((passing / float(total_chips)) * 100)
                     passing_rate[dft_type] = rate_list
-                    print(passing_rate)
+
                 else:
                     rate_list = passing_rate.get(dft_type)
                     rate_list.append(0.0)
                     passing_rate[dft_type] = rate_list
-                    print(passing_rate)
+
                 if freq_mode not in freq_modes:
                     freq_modes.append(freq_mode)
+        total_parts = copy.deepcopy(list(passing_rate.values()))
+        for i in range(len(total_parts)):
+            for j in range(len(total_parts[0])):
+                total_parts[i][j] = int((total_parts[i][j] / 100.00) * total_chips)
+
+        df_passing_rate = pd.DataFrame(np.array(list(passing_rate.values())).T.tolist(),
+                                       columns= list(passing_rate.keys()), index = freq_modes)
+        df_parts_passing = pd.DataFrame(np.array(total_parts).T.tolist(),
+                                       columns=list(passing_rate.keys()), index=freq_modes)
+
+        summary_data = os.path.join(output_dir, "_".join(freq_modes) + " summary data.csv")
+        with open(summary_data, 'w', newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(["Parts Passing Rate (%)"])
+        df_passing_rate.to_csv(summary_data, index=True, sep=',', header=True, mode='a')
+        with open(summary_data, 'a', newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow("")
+            writer.writerow(["# of Parts Passing"])
+        df_parts_passing.to_csv(summary_data, index=True, sep=',', header=True, mode='a')
+
         title = "_".join(freq_modes) + " " + " Passing Percentage"
         plt.title(title)
 
@@ -214,13 +235,26 @@ class PostProcess():
         self.add_labels(list(passing_rate.values()), spacing, True)
 
         plt.ylim(0, 100)
-        # rates = pd.Series(passing_rate)
-        # colors = list('bgrkymc')
-        #
-        # rates.plot(
-        #     kind='bar',
-        #     color=colors,
-        # )
+
+        output_plot = os.path.join(output_dir, title + '.jpg')
+        plt.savefig(output_plot)
+
+        plt.clf()
+
+
+        title = "_".join(freq_modes) + " " + " # of Passing Parts"
+        plt.title(title)
+
+        X_axis = np.arange(len(freq_modes))
+        plt.bar(X_axis - 0.2 , total_parts[0], 0.2, label="INT")
+        plt.bar(X_axis, total_parts[1], 0.2, label="SAF")
+        plt.bar(X_axis + 0.2, total_parts[2], 0.2, label="TDF")
+        plt.xticks(X_axis, freq_modes)
+        plt.xlabel('Pattern Category')
+        plt.ylabel('# of Passing Parts')
+        plt.legend()
+        spacing = [-.2, .8]
+        self.add_labels(total_parts, spacing)
 
         output_plot = os.path.join(output_dir, title + '.jpg')
         plt.savefig(output_plot)
@@ -233,8 +267,7 @@ class PostProcess():
                 else:
                     plt.text(pos[i], value[i], str(round(value[i], 2)), ha='center', fontweight = 'bold')
                 pos[i] = pos[i] + .2
-                print(pos[i])
-                print(value[i])
+
 
     def tdf_shmoo_graph(self):
         print('tdf')
@@ -245,10 +278,10 @@ def main():
     # run = "dft_run_2021-07-07"
     # output_dir = r"G:\ATPG_CDP\pattern_execution\output"
     base_dir = r"C:\Users\rpenmatc\OneDrive - Qualcomm\Desktop"
-    run = "dft_run_2021-07-07"
+    runs = ["dft_run_2021-07-07"]
     output_dir = r"C:\Users\rpenmatc\OneDrive - Qualcomm\Desktop\pattern_execution\output"
     post = PostProcess()
-    #output_file = post.dlog_csv_post_process(base_dir, run, output_dir)
+    #post.dlog_csv_post_process(base_dir, runs, output_dir)
    # post.passing_rate_graph(output_file)
     post.all_data_compiled(output_dir)
 
